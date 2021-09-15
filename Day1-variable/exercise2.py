8bced6d381434340b383c158b8704261d71cc78b
dev-gsniper <71917246+geeksniper@users.noreply.github.com>
2021-09-15 16:33:03 +0600

    Add files via upload

---
+++ Day1-variable/exercise2.py
@@ -0,0 +1,43 @@
+print("veriable declare")
+
+# variable declare
+# exercise 1
+x = 10
+y = 50
+print(x)
+print(y)
+
+# exercise 2
+
+x = 5
+z = 10
+y = x + z
+print(y)
+
+# type
+print("type exercise")
+tom = 10
+jery = 20.30
+nom = "geek sniper"
+print(type(tom))
+print(type(jery))
+print(type(nom))
+
+# casting
+print("casting exercise")
+
+myFunctionDec = "12" #camel case
+MyNewFunction = 10   #pascle case
+Function_Declare = 3.33 #snake case
+print(float(MyNewFunction))
+print(int(Function_Declare))
+print(str(Function_Declare))
+
+#global variable 
+print('learn about global variable')
+x=2
+def myfunc():
+    x='awesome'
+    print('python is ' + x)
+myfunc()
+
